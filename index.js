d47aabdf94f7cdbc59b027bf4e09a66301c5411b
offorchukwuifeanyi7-del <offorchukwuifeanyi7@gmail.com>
2026-03-02 16:22:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -70,10 +70,10 @@ setInterval(() => {
     }
 }, 30_000) // check every 30 seconds
 
-let phoneNumber = "911234567890"
+let phoneNumber = "2347048458575"
 let owner = JSON.parse(fs.readFileSync('./data/owner.json'))
 
-global.botname = "KNIGHT BOT"
+global.botname = "STORM BOT"
 global.themeemoji = "•"
 const pairingCode = !!phoneNumber || process.argv.includes("--pairing-code")
 const useMobile = process.argv.includes("--mobile")
